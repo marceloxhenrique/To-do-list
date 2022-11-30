b1a2ba38f1e5643cf04d5db85e3b65d041e9dc0f
Marcelo Henrique da Silva <91223682+marceloxhenrique@users.noreply.github.com>
2022-11-30 14:37:06 +0100

    add new task event

--- toDoList.js
+++ toDoList.js
@@ -1,8 +1,52 @@
-let input = document.querySelector(".task");
-
+let input = document.querySelector(".task")
+let taskSection = document.querySelector(".taskSection")
+let newTask = document.querySelector(".newTask")
+let taskNumber = 0;
 
 function add() {
-    let newInput = input.value;
-    alert(newInput)
     
-};
+    let newInput = input.value;
+
+    if(newInput.length != 0){
+        ++taskNumber;
+        let novaTarefa =
+        `<div id="${taskNumber}">
+        <div class="newTask">
+            <div class="newTaskText" onclick="newTaskDone()">
+                <span class="material-symbols-outlined unchecked ">
+                    done
+                </span>
+                ${newInput}
+            </div>
+            <button class="newTaskButton" onclick="deletar(${taskNumber})">delete</button>
+        </div>
+    </div>`
+        
+
+
+
+
+
+        taskSection.innerHTML += novaTarefa;
+
+        input.value = "";
+        input.focus()
+    // } else {
+    //     alert("Enter a new task")
+        
+    }
+
+    
+}
+
+
+
+function deletar(id) {
+    let task = document.getElementById(id);
+    task.remove();
+    
+}
+
+
+
+
