dc35735589da6f660dd151eed70d7e0f7124d407
Marcelo Henrique da Silva <91223682+marceloxhenrique@users.noreply.github.com>
2022-12-02 00:41:33 +0100

    Final updates

--- toDoList.js
+++ toDoList.js
@@ -1,43 +1,62 @@
-let input = document.querySelector("#inputTask");
-let newTaskSection = document.querySelector(".newTaskSection");       
+let input = document.querySelector("#inputNewTask")
+let newtaskArea = document.querySelector(".newtask-area") 
 let counter = 0;
+//Adding a new task
+function addNewTask() {
 
-    
-function buttonAddTask() {
-    counter ++;
     let newInput = input.value;
-    newTaskSection.innerHTML +=
-
-    `<div id="${counter}">
-        <div class="newTask">
-            <div class="newTaskTexto">
-                <span class="material-symbols-outlined  unchecked">
-                    check
-                </span>
-                ${newInput}
-                ${counter}
+    if(newInput.length > 0){
+        counter ++;
+        let task = document.createElement("div")
+        task.innerHTML +=
+        `
+            <div id="${counter}" class="newTask unchecked" >
+                <div class="icone-area">
+                    <span class="material-symbols-outlined">
+                        check
+                    </span>
+                </div>
+                <div class="text-area" onclick="check(${counter})">
+                    ${newInput}
+                </div>
+                <div class="button-area">
+                    <button id="buttonDelete" onclick="deleteTask(${counter})">Delete</button>
+                </div>
             </div>
-            <button class="newTaskButton" onclick="deleteTask(${counter})">
-                <span class="material-symbols-outlined trash">
-                    delete
-                </span>
-            Deletar</button>
-        </div>
-    </div>`
+        `    
+
+        newtaskArea.appendChild(task)
+
+        //cleaning and fucousing in the input
+        input.value = "";
+        input.focus()
+    } else {
+        alert("ENTER A NEW TASK")
+    }
     
-    input.focus();
-    input.value = "";
+
 }
+
+input.addEventListener("keyup", function (event){
+    if(event.keyCode == 13){
+        event.preventDefault();
+        addNewTask();
+        
+    }
+})
 
 function deleteTask(id) {
-    document.getElementById(id).remove()
+    let item = document.getElementById(id)
+    item.remove()
 }
 
-input.addEventListener("keyup", function (event) {
-    if(event.keyCode === 13){
-        event.preventDefault();
-        buttonAddTask();
+function check(id) {
+    let check = document.getElementById(id)
+    if(check.classList.contains("unchecked")) {
+        check.classList.replace("unchecked", "checked");
+        newtaskArea.parentNode.appendChild(check);
+    } else {
+        check.classList.replace("checked", "unchecked")
+        newtaskArea.appendChild(check);
     }
-});
-
-
+}
